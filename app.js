562ffbb39075297432d4073840aa53a4386dd7cb
MizaelFelipe <mizaelfelipe@outlook.com>
2023-07-01 02:29:43 -0300

    Realizando a chamada da API e adicionando a tratativa caso de erro

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 const getTypeColor = type => {
-    const normal = '#F5F5F5'
-    return {
-      normal,
-      fire: '#FDDFDF',
-      grass: '#DEFDE0',
-      electric: '#FCF7DE',
-      ice: '#DEF3FD',
-      water: '#DEF3FD',
-      ground: '#F4E7DA',
-      rock: '#D5D5D4',
-      fairy: '#FCEAFF',
-      poison: '#98D7A5',
-      bug: '#F8D5A3',
-      ghost: '#CAC0F7',
-      dragon: '#97B3E6',
-      psychic: '#EAEDA1',
-      fighting: '#E6E0D4'
-    }[type] || normal
+  const normal = '#F5F5F5'
+  return {
+    normal,
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    ice: '#DEF3FD',
+    water: '#DEF3FD',
+    ground: '#F4E7DA',
+    rock: '#D5D5D4',
+    fairy: '#FCEAFF',
+    poison: '#98D7A5',
+    bug: '#F8D5A3',
+    ghost: '#CAC0F7',
+    dragon: '#97B3E6',
+    psychic: '#EAEDA1',
+    fighting: '#E6E0D4'
+  }[type] || normal
+}
+
+const handlePageLoaded = async () => {
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=15&offset=0')
+
+    if (!response.ok) {
+      throw Error("nao foi possivel obter as informacoes")
+    }
+    const { results: pokeApiResults } = await response.json()
+
+
+    console.log(pokeApiResults);
+    console.log("Deu certo ");
+
+  } catch (error) {
+    alert("Algo deu errado")
   }
-  
+
+}
+
+handlePageLoaded()
