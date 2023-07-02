f01390bc950bac2aca32fd68fc9115e99b27929d
MizaelFelipe <mizaelfelipe@outlook.com>
2023-07-02 02:54:11 -0300

    realizando requisicao de pegar type e Ids dos pokemons, realizado tambem a exportacao para uma funcao para identar o codigo e isolar as funcionalidades

--- app.js
+++ app.js
@@ -19,6 +19,22 @@ const getTypeColor = type => {
   }[type] || normal
 }
 
+const getPokemonsType = async pokeApiResults => {
+  const promises = pokeApiResults.map(result => fetch(result.url))
+  const responses = await Promise.allSettled(promises)
+  const fulfilled = responses.filter(response => response.status === "fulfilled")
+  const pokePromises = fulfilled.map(url => url.value.json())
+  const pokemons = await Promise.all(pokePromises)
+  return pokemons.map(fulfilled => fulfilled.types.map(info => info.type.name))
+
+}
+
+const getPokemonsIds = pokeApiResults => pokeApiResults.map(({ url }) => {
+  const urlAsArray = url.split("/")
+  return urlAsArray.at(urlAsArray.length - 2)
+})
+
+
 const handlePageLoaded = async () => {
   try {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=15&offset=0')
@@ -26,11 +42,14 @@ const handlePageLoaded = async () => {
     if (!response.ok) {
       throw Error("nao foi possivel obter as informacoes")
     }
+
     const { results: pokeApiResults } = await response.json()
 
+    const types = await getPokemonsType(pokeApiResults)
+    const ids = getPokemonsIds(pokeApiResults)
 
-    console.log(pokeApiResults);
-    console.log("Deu certo ");
+
+      console.log(ids);
 
   } catch (error) {
     alert("Algo deu errado")
